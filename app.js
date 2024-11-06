03e67377bb2683ffd14265be2e94ef3c94a31a75
Matheus Francisco <matheusfssilva2001@gmail.com>
2024-11-06 17:55:36 -0300

    ajustes nas variaveis de ambiente

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.use('/', tarefaRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://127.0.0.1:${PORT}`);
 });
--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || '3306',
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
