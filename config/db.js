9b979b28cdefdc1355c891700d2bb3534da7d605
Matheus Francisco <matheusfssilva2001@gmail.com>
2024-11-06 20:46:38 -0300

    Ajuste no db

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || '3306',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
