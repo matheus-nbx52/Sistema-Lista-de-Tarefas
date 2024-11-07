df4e94b934104f3a35603fe15c0e8b993befc12a
Matheus Francisco <matheusfssilva2001@gmail.com>
2024-11-07 11:34:33 -0300

    correções

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 10000,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
